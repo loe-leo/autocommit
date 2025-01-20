9ddba61870df43656ca6c5429d77a501e10901a7
SiravichU <siravichU@gmail.com>
2025-01-20 21:17:08 +0700

    autocommit

---
+++ generate_data.py
@@ -0,0 +1,17 @@
+import json
+from datetime import datetime
+
+# Get the current time
+current_time = datetime.now().isoformat()
+
+# Data to write into the JSON file
+data = {"current_time": current_time}
+
+# Specify the filename
+filename = "data.json"
+
+# Write the data to a JSON file
+with open(filename, "w") as json_file:
+    json.dump(data, json_file, indent=4)
+
+print(f"Current time written to {filename}: {current_time}")
